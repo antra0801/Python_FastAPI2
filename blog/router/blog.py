436f3554766bffb7683ed1ca0afc59fabbf0745b
Antra Gupta <antragupta@bdo.in>
2025-08-11 12:04:30 +0530

    migrated all the blog routes in the router/ blog file

--- blog/router/blog.py
+++ blog/router/blog.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter , status, Depends
+from fastapi import APIRouter , status, Depends, Response , HTTPException
 from typing import List
 from sqlalchemy.orm import Session
 from .. import schemas, database, model
@@ -12,3 +12,60 @@ def allBlogs(db : Session = Depends(database.get_db)):
     blogs = db.query(model.Blog).all()
     # print(blogs)
     return blogs
+
+
+@router.post('/blog1', status_code= status.HTTP_201_CREATED , tags=['blogs'])
+def create(request_body : schemas.BlogClass, db : Session = Depends(database.get_db)):
+    new_blog = model.Blog(title = request_body.title , body = request_body.body, userId = 1)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+
+
+@router.delete('/blog/{id}',status_code = status.HTTP_204_NO_CONTENT,tags=['blogs'])
+def deleteBlog(id, db : Session = Depends(database.get_db)):
+    blogQuery = db.query(model.Blog).filter(model.Blog.id == id)
+    if not blogQuery.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"blog with id {id} not found.")
+    blogQuery.delete(synchronize_session=False)
+    db.commit()
+    return {'detail':'id has deleted.'} 
+
+
+
+
+@router.put('/blogUpdate/{id}', status_code=status.HTTP_202_ACCEPTED , tags=['blogs'])
+def update(id , request_body : schemas.BlogClass , db : Session = Depends(database.get_db)):
+    print(request_body)
+    blogQuery = db.query(model.Blog).filter(model.Blog.id == id)
+    if not blogQuery.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"blog with id {id} not found.")
+    blogQuery.update(request_body.dict())
+    print(request_body.dict())
+    db.commit()
+    return"updated"
+    # except Exception as e:
+    #     raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR , detail=f"Update failed: {str(e)}")
+
+# @router.get('/get-all-blogs', status_code=status.HTTP_200_OK , response_model= List[schemas.ShowBlog] , tags=['blogs'])
+# def allBlogs(db : Session = Depends(get_db)):
+#     blogs = db.query(model.Blog).all()
+#     # print(blogs)
+#     return blogs
+
+
+@router.get('/get-blogs-by-id/{id}' , status_code=200, response_model=schemas.ShowBlog ,tags=['blogs'] )
+def getData(id, response: Response, db : Session = Depends(database.get_db)):
+    # data = db.query(model.Blog).filter(model.Blog.id == id).first().body✅
+    data = db.query(model.Blog).filter(model.Blog.id == id).first()
+
+    if not data:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=  f'Blog with the {id} is not available'
+        )
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {'detail' : f'Blog with the {id} is nor available'}
+    return data
