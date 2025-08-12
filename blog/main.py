04c24d998be716939ff008127b28574ea229ff0b
Antra Gupta <antragupta@bdo.in>
2025-08-12 10:56:40 +0530

    added user routes in router/user.py

--- blog/main.py
+++ blog/main.py
@@ -5,8 +5,8 @@ from sqlalchemy.orm import Session
 from typing import List
 from .hashing import Hash
 # from passlib.context import CryptContext
-from .router import blog
-
+from .router import blog  , user
+ 
 app = FastAPI()
 
 
@@ -22,29 +22,5 @@ model.Base.metadata.create_all(engine)
 
 app.include_router(blog.router)
 
+app.include_router(user.router)
 
-# pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-
-@app.post('/user' , response_model=schemas.ShowUser , tags=['users'])
-def create_user(request_body : schemas.User , db : Session = Depends(get_db)): 
-    # hashedPassword = pwd_context.hash(request_body.password)
-    newUser = model.User(name=request_body.name , email = request_body.email , password = Hash.bcrpyt(request_body.password))
-    db.add(newUser)
-    db.commit()
-    db.refresh(newUser)
-    return newUser
-
-@app.get('/user/{id}' , response_model=schemas.ShowUser, tags=['users'])
-def get_user(id:int , db : Session = Depends(get_db)):
-    user = db.query(model.User).filter(model.User.id == id).first()
-
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with {id} id not found.")
-    
-    return user
-
-@app.get('/all-users' , tags=['users'])
-def allUsers(db : Session=Depends(get_db)):
-    all_users_from_db = db.query(model.User).all()
-    return all_users_from_db 
--- blog/router/user.py
+++ blog/router/user.py
@@ -1,3 +1,36 @@
-from fastapi import APIRouter
+from fastapi import APIRouter , Depends , HTTPException , status
+from sqlalchemy.orm import  Session 
 from typing import List
-from .. import schemas, database
+from .. import schemas, database, model 
+from ..hashing import Hash
+
+# hash_pass = hashing.Hash()
+
+router = APIRouter()
+
+get_db = database.get_db
+# pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+@router.post('/user' , response_model=schemas.ShowUser , tags=['users'])
+def create_user(request_body : schemas.User , db : Session = Depends(get_db)): 
+    # hashedPassword = pwd_context.hash(request_body.password)
+    newUser = model.User(name=request_body.name , email = request_body.email , password = Hash.bcrpyt(request_body.password))
+    db.add(newUser)
+    db.commit()
+    db.refresh(newUser)
+    return newUser
+
+@router.get('/user/{id}' , response_model=schemas.ShowUser, tags=['users'])
+def get_user(id:int , db : Session = Depends(get_db)):
+    user = db.query(model.User).filter(model.User.id == id).first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with {id} id not found.")
+    
+    return user
+
+@router.get('/all-users' , tags=['users'])
+def allUsers(db : Session=Depends(get_db)):
+    all_users_from_db = db.query(model.User).all()
+    return all_users_from_db 
